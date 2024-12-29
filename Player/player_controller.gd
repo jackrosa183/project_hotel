fdcad1f17fdbb8a3447be339434c248c92ea8309
Jack Rosa <jack.rosa@hashrocket.com>
2024-12-29 17:34:45 -0500

    feat: blend between walk and idle animations

--- Player/player_controller.gd
+++ Player/player_controller.gd
@@ -7,10 +7,27 @@ const JUMP_VELOCITY = 4.5
 @onready var player_cam: Camera3D = $CamRoot/CamYaw/CamPitch/SpringArm3D/Camera3D
 @onready var armature: Node3D = $Armature
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 
 @export var look_sensitivity = 1.0
 @export var lerp_val = .1
+@export var anim_blend_speed = 15
+
+var walk_val = 0
+enum {IDLE,WALK}
+var current_anim = IDLE
+
+func handle_animation(delta):
+	match current_anim:
+		IDLE:
+			walk_val = lerpf(walk_val, 0, anim_blend_speed * delta)
+		WALK:
+			walk_val = lerpf(walk_val, 1, anim_blend_speed * delta)
+	update_tree()
+
+func update_tree():
+	animation_tree["parameters/Walk/blend_amount"] = walk_val
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED	
@@ -22,7 +39,8 @@ func _input(event):
 		cam_pitch.rotation.x = clamp(cam_pitch.rotation.x, deg_to_rad(-90), deg_to_rad(45))
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	handle_animation(delta)
+	# Add gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -39,13 +57,14 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction = direction.rotated(Vector3.UP, cam_yaw.rotation.y)
+	
 	if direction:
-		animation_player.play("walk")
+		current_anim = WALK
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), lerp_val)
 	else:
-		animation_player.play("idle")
+		current_anim = IDLE
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
